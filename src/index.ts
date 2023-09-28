7b3590bd8cf725b053f80904f9154ada9783e51a
Wongsatorn Pornpirunroch <wongsatorn.fw@gmail.com>
2023-09-28 14:50:19 +0700

    fix: cors options

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,18 @@
 import express from "express";
-import cors from "cors"
+import cors from "cors";
 import cookieParser from "cookie-parser";
 import usersRoute from "./routes/user.route";
 
 const app = express();
 
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 app.use(express.json());
-app.use(cors())
-app.use(cookieParser())
+app.use(cookieParser());
 
 app.use("/api/users", usersRoute);
 
